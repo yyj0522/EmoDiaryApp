82a1b3daad715527ef27d78014dd865381d69076
yyj0522 <aha2408@naver.com>
2025-06-07 01:25:37 +0900

    수정한 내용 간단히 작성

--- app/src/main/java/com/example/myapplication/Settings.java
+++ app/src/main/java/com/example/myapplication/Settings.java
@@ -2,15 +2,40 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class Settings extends AppCompatActivity {
+
+    private static boolean isDarkMode = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
         setContentView(R.layout.settingsactivity);
 
         Button btnToMain = findViewById(R.id.btn_to_main);
         btnToMain.setOnClickListener(v -> finish());
+
+        Button btnTheme = findViewById(R.id.btn_theme);
+        btnTheme.setOnClickListener(v -> {
+            isDarkMode = !isDarkMode;
+
+            if (isDarkMode) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+
+            recreate();
+        });
     }
 }
